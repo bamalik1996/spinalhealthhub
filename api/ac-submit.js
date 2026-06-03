d8cc490c21ff02bef703db7c291ab87cd345a614
Abdul Hameed Malik <88552973+ah-malik@users.noreply.github.com>
2026-06-03 13:30:45 +0500

    Update ac-submit.js

--- api/ac-submit.js
+++ api/ac-submit.js
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
 
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://spinal-health-hub.webflow.io"
+    "https://www.spinalhealthhub.com.au"
   );
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
